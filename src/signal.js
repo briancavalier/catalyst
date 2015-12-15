6c81e4328d75ff1b8ecc1bc8cc9d4413f5cf5b15
Brian Cavalier <brian@hovercraftstudios.com>
2015-12-15 08:55:07 -0500

    Cleanup

--- src/signal.js
+++ src/signal.js
@@ -1,58 +1,5 @@
 import { map as mapE } from './event';
 
-//const id = x => x;
-
-// type Signal t a :: t -> (a, Signal t a)
-
-//const cacheSignal = (s, value) =>
-//    t => value === undefined ? (value = s(t)) : value;
-//
-//// newSignal :: (t -> a) -> t -> Signal t a
-//const newSignal = f => cacheSignal(t => makePair(f(t), newSignal(f)));
-//
-//// time :: Signal t t
-//export const time = newSignal(id);
-//
-//// constant :: a -> Signal t a
-//export const constant = value => {
-//    const next = t => ({ value, next });
-//    return next;
-//};
-//
-//// map :: (a -> b) -> Signal t a -> Signal t b
-//export const map = (f, s) => cacheSignal(t => mapNext(f, s(t)));
-//
-//const mapNext = (f, { value, next }) => makePair(f(value), map(f, next));
-//
-//// liftA2 :: (a -> b -> c) -> Signal t a -> Signal t b -> Signal t c
-//export const liftA2 = (f, s1, s2) =>
-//    cacheSignal(t => liftA2Next(f, s1(t), s2(t)));
-//
-//const liftA2Next = (f, { value: v1, next: n1 }, { value: v2, next: n2 }) =>
-//    makePair(f(v1, v2), liftA2(f, n1, n2));
-//
-//// step :: a -> Event t a -> Signal t a
-//export const step = (x, e) =>
-//    switcher(constant(x), mapE(constant, e));
-//
-//// switcher :: Signal t a -> Event t (Signal t a) -> Signal t a
-//export const switcher = (inits, e) =>
-//    cacheSignal(t => stepET(inits, e(t), t));
-//
-//const stepET = (s, ev, t) =>
-//    ev.time <= t ? switchTo(ev.value, t) : stayAt(s(t), ev);
-//
-//const stepE = (s, ev) => cacheSignal(t => stepET(s, ev, t));
-//
-//const stayAt = (sv, ev) => makePair(sv.value, stepE(sv.next, ev));
-//
-//const switchTo = ({ value, next }, t) => switchToS(value(t), next);
-//
-//const switchToS = ({ value, next }, e) => makePair(value, switcher(next, e));
-//
-//
-//const makePair = (value, next) => ({ value, next });
-
 class Signal {
     constructor(runSignal) {
         this._runSignal = runSignal;
@@ -68,7 +15,7 @@ class Signal {
     map(f) {
         return new Signal(t => {
             const { value, next } = this.runSignal(t);
-            return { value: f(value), next: next.map(f) };
+            return makePair(f(value), next.map(f));
         });
     }
 
@@ -80,7 +27,7 @@ class Signal {
         return new Signal(t => {
             const { value: v1, next: n1 } = this.runSignal(t);
             const { value: v2, next: n2 } = b.runSignal(t);
-            return { value: f(v1, v2), next: n1.liftA2(f, n2) }
+            return makePair(f(v1, v2), n1.liftA2(f, n2))
         });
     }
 }
