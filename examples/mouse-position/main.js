d5c12999b9d4732f2bf3c50979d5d085b16f07d8
Brian Cavalier <brian@hovercraftstudios.com>
2016-01-13 13:11:18 -0500

    Update mouse-position example

--- examples/mouse-position/main.js
+++ examples/mouse-position/main.js
@@ -3,19 +3,21 @@ import { build, runEvent } from '../../src/source';
 import { domEvent } from '../../src/dom';
 import { map, sample, merge } from '../../src/event';
 import { liftA2, step } from '../../src/signal';
+import { newClock } from '../../src/clock';
 
 const join = sep => (a, b) => a + sep + b;
+const render = e => document.body.innerHTML = e;
 
-const n = build(function* () {
+const updates = build(function* (window) {
     const mouse = yield domEvent('mousemove', window);
     const keydown = yield domEvent('keydown', window);
 
-    const coords = step('hi', map(e => `${e.clientX},${e.clientY}`, mouse));
-    const keyCode = step(0, map(e => e.keyCode, keydown));
+    const coords = step('-,-', map(e => `${e.clientX},${e.clientY}`, mouse));
+    const keyCode = step('-', map(e => e.keyCode, keydown));
 
     const s = liftA2(join(':'), coords, keyCode);
 
     return sample(s, merge(mouse, keydown));
-});
+}, window);
 
-runEvent(e => document.body.innerHTML = e, n, Date.now);
+runEvent(render, updates, newClock(Date.now));
